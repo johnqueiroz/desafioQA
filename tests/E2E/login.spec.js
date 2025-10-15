2d4280b0c6d60d0958e7cb10fdba32c2dea7a3b5
John Emerson <john.emerson@atlastechnol.com>
2025-10-15 17:58:48 -0300

    feat: added tests of sucessCases and failure case

--- tests/E2E/login.spec.js
+++ tests/E2E/login.spec.js
@@ -3,14 +3,35 @@ import { test } from '../fixture'
 import { users } from '../../utils/users'
 import { routes } from '../../utils/routes'
 
+const dataErros = {
+  locked: 'Epic sadface: Sorry, this user has been locked out.',
+}
+
 test.beforeEach('Acessar página do saucedemo', async ({ loginPage }) => {
   await loginPage.visit()
 })
 
-test.describe('Fluxo de login padrão', () => {
-  test('deve validar o fluxo de login padrão', {
-  }, async ({ loginPage, page }) => {
-    await loginPage.doLogin(users.standard, 'secret_sauce')
-    await expect(page).toHaveURL(routes.inventory)
+test.describe('Fluxo de login', () => {
+
+  const successCases = [
+    { username: 'standard', user: users.standard },
+    { username: 'problem', user: users.problem },
+    { username: 'performance', user: users.performance },
+    { username: 'error', user: users.error },
+    { username: 'visual', user: users.visual },
+  ]
+
+  for (const { username, user } of successCases) {
+    test(`deve validar o fluxo de login com sucesso - user ${username}`, {
+    }, async ({ loginPage, page }) => {
+      await loginPage.doLogin(user, 'secret_sauce')
+      await expect(page).toHaveURL(routes.inventory)
+    })
+  }
+
+  test('deve validar o fluxo de login para user locked out', {
+  }, async ({ loginPage }) => {
+    await loginPage.doLogin(users.locked, 'secret_sauce')
+    await expect(loginPage.lockedError).toContainText(dataErros.locked)
   })
 })
