1d0345b4b5a68f9a009ceee1780771e1724d13b2
John Emerson <john.emerson@atlastechnol.com>
2025-10-15 16:56:21 -0300

    feat: utils files

---
+++ utils/users.js
@@ -0,0 +1,8 @@
+export const users = {
+    standard: 'standard_user',
+    locked: 'locked_out_user',
+    problem: 'problem_user',
+    performance: 'performance_glitch_user',
+    error: 'error_user',
+    visual: 'visual_user'
+}
