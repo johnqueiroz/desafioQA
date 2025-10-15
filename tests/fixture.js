da9516573b74f65481818052ec034fda20760a03
johnqueiroz <johnemerson67@gmail.com>
2025-10-15 19:00:16 -0300

    feat: add fixture of product item

--- tests/fixture.js
+++ tests/fixture.js
@@ -1,6 +1,7 @@
 import { test as fixture } from '@playwright/test'
 import { LoginPage } from './pages/LoginPage'
 import { ProductsPage } from './pages/ProductsPage'
+import { ProductItemPage } from './pages/ProductItemPage'
 
 export const test = fixture.extend({
 	loginPage: async ({ page }, use) => {
@@ -8,6 +9,9 @@ export const test = fixture.extend({
 	},
 	productsPage: async ({ page }, use) => {
 		await use(new ProductsPage(page))
+	},
+	productItemPage: async ({ page }, use) => {
+		await use(new ProductItemPage(page))
 	},
 })
 export default test
