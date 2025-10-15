15be94ab8784607c8e82a74eeb9ac580f584e772
John Emerson <john.emerson@atlastechnol.com>
2025-10-15 17:59:53 -0300

    feat: add fixture of product page

--- tests/fixture.js
+++ tests/fixture.js
@@ -1,9 +1,13 @@
 import { test as fixture } from '@playwright/test'
 import { LoginPage } from './pages/LoginPage'
+import { ProductPage } from './pages/ProductPage'
 
 export const test = fixture.extend({
 	loginPage: async ({ page }, use) => {
 		await use(new LoginPage(page))
+	},
+	productPage: async ({ page }, use) => {
+		await use(new ProductPage(page))
 	},
 })
 export default test
